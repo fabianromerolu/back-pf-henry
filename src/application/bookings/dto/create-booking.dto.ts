92a56c708ce7773bee6b37d2e226c6d5d3050bf1
MenHec95 <hectormendez_95@yahoo.com>
2025-10-26 14:23:54 -0300

    feat: creacion de DTO create, limpieza de endpoints

--- src/application/bookings/dto/create-booking.dto.ts
+++ src/application/bookings/dto/create-booking.dto.ts
@@ -1,1 +1,19 @@
-export class CreateBookingDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBookingDto {
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  pinId: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  start_date: Date;
+
+  @IsDate()
+  @IsNotEmpty()
+  end_date: Date;
+}
